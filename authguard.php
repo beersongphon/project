8f6c5539413fcc67280af85c9972b97dfe4ba2d8
Beer <beersongphon2010@gmail.com>
2022-02-23 21:54:49 +0700

    fix code

--- authguard.php
+++ authguard.php
@@ -1,6 +1,9 @@
 <?php
-if($_SESSION['permission_id'] != "1" & $_SESSION['permission_id'] != "2") {
-	header("location: ./index.php");
-	exit();
+if (isset($_SESSION["user_username"])) {
+} else {
+  if($_SESSION['permission_id'] != "1" & $_SESSION['permission_id'] != "2") {
+    header("location: ./index.php");
+    exit();
+  }
 }
 ?>
